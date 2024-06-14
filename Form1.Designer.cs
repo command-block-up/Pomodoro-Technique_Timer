e5f9638d330c5f4b43e59528ed127bdf78a91e81
command-block-up <aishidadingdan@163.com>
2024-06-14 12:09:35 +0800

    添加项目文件。

---
+++ Form1.Designer.cs
@@ -0,0 +1,158 @@
+﻿namespace Pomodoro_Technique
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.progressBar = new System.Windows.Forms.ProgressBar();
+            this.start_button = new System.Windows.Forms.Button();
+            this.stop_button = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("字体圈欣意冠黑体", 42F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label1.Location = new System.Drawing.Point(272, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(253, 84);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "25:00";
+            // 
+            // progressBar
+            // 
+            this.progressBar.Location = new System.Drawing.Point(62, 96);
+            this.progressBar.Name = "progressBar";
+            this.progressBar.RightToLeftLayout = true;
+            this.progressBar.Size = new System.Drawing.Size(693, 23);
+            this.progressBar.TabIndex = 1;
+            // 
+            // start_button
+            // 
+            this.start_button.Location = new System.Drawing.Point(255, 368);
+            this.start_button.Name = "start_button";
+            this.start_button.Size = new System.Drawing.Size(132, 70);
+            this.start_button.TabIndex = 2;
+            this.start_button.Text = "开始";
+            this.start_button.UseVisualStyleBackColor = true;
+            this.start_button.Click += new System.EventHandler(this.start_button_Click);
+            // 
+            // stop_button
+            // 
+            this.stop_button.Location = new System.Drawing.Point(393, 368);
+            this.stop_button.Name = "stop_button";
+            this.stop_button.Size = new System.Drawing.Size(132, 70);
+            this.stop_button.TabIndex = 2;
+            this.stop_button.Text = "停止";
+            this.stop_button.UseVisualStyleBackColor = true;
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 15;
+            this.listBox1.Location = new System.Drawing.Point(62, 157);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(380, 169);
+            this.listBox1.TabIndex = 3;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(449, 157);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(150, 80);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "button3";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(449, 246);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(150, 80);
+            this.button4.TabIndex = 4;
+            this.button4.Text = "button3";
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(605, 157);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(150, 80);
+            this.button5.TabIndex = 4;
+            this.button5.Text = "button3";
+            this.button5.UseVisualStyleBackColor = true;
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(605, 246);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(150, 80);
+            this.button6.TabIndex = 4;
+            this.button6.Text = "button3";
+            this.button6.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.stop_button);
+            this.Controls.Add(this.start_button);
+            this.Controls.Add(this.progressBar);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ProgressBar progressBar;
+        private System.Windows.Forms.Button start_button;
+        private System.Windows.Forms.Button stop_button;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+    }
+}
+
