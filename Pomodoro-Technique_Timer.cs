aaf6d81d1d38bc3b0d2e8b1725a56d3f2e758f8f
command-block-up <aishidadingdan@163.com>
2024-06-14 13:09:07 +0800

    计时功能实现

--- Pomodoro-Technique_Timer.cs
+++ Pomodoro-Technique_Timer.cs
@@ -1,19 +1,49 @@
 ﻿using System;
+using System.Windows.Forms;
 
 namespace Pomodoro_Technique
 {
     public partial class Form : System.Windows.Forms.Form
     {
-        private System.Windows.Forms.Timer timerUpdateProgress;
+        private Timer countdownTimer; // 倒计时定时器
+        private int totalSeconds = 1500;
+        private Timer timerUpdateProgress;
 
         public Form()
         {
             InitializeComponent();
+            InitializeCountdownTimer();
             // 初始化Timer
             timerUpdateProgress = new System.Windows.Forms.Timer();
             timerUpdateProgress.Interval = 60000; // 每隔60000毫秒（即60秒）触发一次Tick事件
             timerUpdateProgress.Tick += TimerUpdateProgress_Tick; // 设置Tick事件的处理方法
         }
+
+        private void InitializeCountdownTimer()
+        {
+            countdownTimer = new Timer();
+            countdownTimer.Interval = 1000; // 每隔1000毫秒（即1秒）触发一次Tick事件
+            countdownTimer.Tick += CountdownTimer_Tick;
+        }
+
+        private void CountdownTimer_Tick(object sender, EventArgs e)
+        {
+            totalSeconds--; // 时间递减1秒
+            if (totalSeconds <= 0)
+            {
+                // 倒计时结束，停止定时器
+                countdownTimer.Stop();
+                label1.Text = "Time's up!";
+            }
+            else
+            {
+                // 计算剩余的分钟和秒，并更新Label
+                int minutes = totalSeconds / 60;
+                int seconds = totalSeconds % 60;
+                label1.Text = $"{minutes:D2}:{seconds:D2}"; // D2保证了即使是个位数也会显示为两位数，如01:05
+            }
+        }
+
 
         private void TimerUpdateProgress_Tick(object sender, EventArgs e)
         {
@@ -21,7 +51,6 @@ namespace Pomodoro_Technique
             if (progressBar.Value < 100)
             {
                 progressBar.Value += 4; // 每次增加4，根据需要调整
-                Console.WriteLine("1 minute pasts");
             }
             else
             {
@@ -33,6 +62,7 @@ namespace Pomodoro_Technique
         private void start_button_Click(object sender, EventArgs e)
         {
             // 开始计时器
+            countdownTimer.Start();
             timerUpdateProgress.Start();
         }
     }
