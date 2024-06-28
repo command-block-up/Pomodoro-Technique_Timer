8d9c60af53a06613c18cdd67e00cb170bdfd958d
command-block-up <aishidadingdan@163.com>
2024-06-28 20:58:03 +0800

    自动调整装口布局；移除未开发的功能；删除无用冗余代码

--- Pomodoro-Technique_Timer.Designer.cs
+++ Pomodoro-Technique_Timer.Designer.cs
@@ -28,23 +28,17 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
             this.label1 = new System.Windows.Forms.Label();
             this.progressBar = new System.Windows.Forms.ProgressBar();
             this.start_button = new System.Windows.Forms.Button();
-            this.listBox1 = new System.Windows.Forms.ListBox();
-            this.button3 = new System.Windows.Forms.Button();
-            this.button4 = new System.Windows.Forms.Button();
-            this.button5 = new System.Windows.Forms.Button();
-            this.button6 = new System.Windows.Forms.Button();
-            this.notifyIcon = new System.Windows.Forms.NotifyIcon(this.components);
             this.SuspendLayout();
             // 
             // label1
             // 
+            this.label1.Anchor = System.Windows.Forms.AnchorStyles.Top;
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("字体圈欣意冠黑体", 42F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
-            this.label1.Location = new System.Drawing.Point(272, 0);
+            this.label1.Location = new System.Drawing.Point(274, 0);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(253, 84);
             this.label1.TabIndex = 0;
@@ -52,6 +46,8 @@
             // 
             // progressBar
             // 
+            this.progressBar.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
             this.progressBar.Location = new System.Drawing.Point(62, 96);
             this.progressBar.Maximum = 1500;
             this.progressBar.Name = "progressBar";
@@ -61,7 +57,9 @@
             // 
             // start_button
             // 
-            this.start_button.Location = new System.Drawing.Point(328, 368);
+            this.start_button.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.start_button.Location = new System.Drawing.Point(334, 368);
             this.start_button.Name = "start_button";
             this.start_button.Size = new System.Drawing.Size(132, 70);
             this.start_button.TabIndex = 2;
@@ -69,68 +67,11 @@
             this.start_button.UseVisualStyleBackColor = true;
             this.start_button.Click += new System.EventHandler(this.start_button_Click);
             // 
-            // listBox1
-            // 
-            this.listBox1.FormattingEnabled = true;
-            this.listBox1.ItemHeight = 15;
-            this.listBox1.Location = new System.Drawing.Point(62, 157);
-            this.listBox1.Name = "listBox1";
-            this.listBox1.Size = new System.Drawing.Size(380, 169);
-            this.listBox1.TabIndex = 3;
-            // 
-            // button3
-            // 
-            this.button3.Location = new System.Drawing.Point(449, 157);
-            this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(150, 80);
-            this.button3.TabIndex = 4;
-            this.button3.Text = "button3";
-            this.button3.UseVisualStyleBackColor = true;
-            // 
-            // button4
-            // 
-            this.button4.Location = new System.Drawing.Point(449, 246);
-            this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(150, 80);
-            this.button4.TabIndex = 4;
-            this.button4.Text = "button3";
-            this.button4.UseVisualStyleBackColor = true;
-            // 
-            // button5
-            // 
-            this.button5.Location = new System.Drawing.Point(605, 157);
-            this.button5.Name = "button5";
-            this.button5.Size = new System.Drawing.Size(150, 80);
-            this.button5.TabIndex = 4;
-            this.button5.Text = "button3";
-            this.button5.UseVisualStyleBackColor = true;
-            // 
-            // button6
-            // 
-            this.button6.Location = new System.Drawing.Point(605, 246);
-            this.button6.Name = "button6";
-            this.button6.Size = new System.Drawing.Size(150, 80);
-            this.button6.TabIndex = 4;
-            this.button6.Text = "button3";
-            this.button6.UseVisualStyleBackColor = true;
-            // 
-            // notifyIcon
-            // 
-            this.notifyIcon.BalloonTipIcon = System.Windows.Forms.ToolTipIcon.Info;
-            this.notifyIcon.BalloonTipText = "番茄工作法";
-            this.notifyIcon.Text = "番茄工作法";
-            this.notifyIcon.Visible = true;
-            // 
             // PomodoroForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.button6);
-            this.Controls.Add(this.button4);
-            this.Controls.Add(this.button5);
-            this.Controls.Add(this.button3);
-            this.Controls.Add(this.listBox1);
             this.Controls.Add(this.start_button);
             this.Controls.Add(this.progressBar);
             this.Controls.Add(this.label1);
@@ -146,12 +87,6 @@
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.ProgressBar progressBar;
         private System.Windows.Forms.Button start_button;
-        private System.Windows.Forms.ListBox listBox1;
-        private System.Windows.Forms.Button button3;
-        private System.Windows.Forms.Button button4;
-        private System.Windows.Forms.Button button5;
-        private System.Windows.Forms.Button button6;
-        private System.Windows.Forms.NotifyIcon notifyIcon;
     }
 }
 
